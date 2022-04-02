198b51aee25fc9c1ce4161ed6645da48fa5a52ca
anantha99 <1by18te005@bmsit.in>
2022-04-02 17:41:08 +0530

    calculator program in c

---
+++ program/calculator.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "unity.h"
+
+int addition(int param1, int param2){
+    int Sum = param1 + param2;
+    return Sum;
+}
+int subtraction(int param1, int param2){
+    int Sub = param1 - param2;
+    return Sub;
+}
+int multiplication(int param1, int param2){
+    int mul = param1 * param2;
+    return mul;
+}
+int division(int param1, int param2){
+    int div = param1 / param2;
+    return div;
+}
+
+
+
+
+
+#if 0
+int main(){
+    return 0;
+}
+#endif
